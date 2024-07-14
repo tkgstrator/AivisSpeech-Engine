9bd0011dfdf923989d8bcc141af478ceb62adadd
tsukumi <tsukumijima@users.noreply.github.com>
2024-07-14 21:33:44 +0900

    Fix: ビルド後の AivisSpeech Engine の起動確認に失敗した場合でも確実に起動したプロセスが終了されるように修正

--- build_util/check_release_build.py
+++ build_util/check_release_build.py
@@ -27,50 +27,57 @@ def test_release_build(dist_dir: Path, skip_run_process: bool) -> None:
         process = Popen([run_file.absolute()], cwd=dist_dir)
 
     # 起動待機
-    for i in range(10):
-        print(f"Waiting for the engine to start... {i}")
-        time.sleep(15)
-        try:
-            req = Request(base_url + "version")
-            with urlopen(req) as res:
-                if len(res.read()) > 0:
-                    break
-        except Exception:
-            continue
-    else:
-        print("Failed to start the engine.")
-        exit(1)
+    try:
+        for i in range(10):
+            print(f"Waiting for the engine to start... {i}")
+            time.sleep(15)
+            try:
+                req = Request(base_url + "version")
+                with urlopen(req) as res:
+                    if len(res.read()) > 0:
+                        break
+            except Exception:
+                continue
+        else:
+            print("Failed to start the engine.")
+            raise Exception("Failed to start the engine.")
 
-    # テキスト -> クエリ
-    text = "こんにちは、音声合成の世界へようこそ"
-    req = Request(
-        base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
-        method="POST",
-    )
-    with urlopen(req) as res:
-        query = json.loads(res.read().decode("utf-8"))
+        # テキスト -> クエリ
+        text = "こんにちは、音声合成の世界へようこそ"
+        req = Request(
+            base_url + "audio_query?" + urlencode({"speaker": "1", "text": text}),
+            method="POST",
+        )
+        with urlopen(req) as res:
+            query = json.loads(res.read().decode("utf-8"))
 
-    # クエリ -> 音声
-    req = Request(base_url + "synthesis?speaker=1", method="POST")
-    req.add_header("Content-Type", "application/json")
-    req.data = json.dumps(query).encode("utf-8")
-    with urlopen(req) as res:
-        wave = res.read()
-    soundfile.read(BytesIO(wave))
+        # クエリ -> 音声
+        req = Request(base_url + "synthesis?speaker=1", method="POST")
+        req.add_header("Content-Type", "application/json")
+        req.data = json.dumps(query).encode("utf-8")
+        with urlopen(req) as res:
+            wave = res.read()
+        soundfile.read(BytesIO(wave))
 
-    # エンジンマニフェスト
-    req = Request(base_url + "engine_manifest", method="GET")
-    with urlopen(req) as res:
-        manifest = json.loads(res.read().decode("utf-8"))
-        assert "uuid" in manifest
+        # エンジンマニフェスト
+        req = Request(base_url + "engine_manifest", method="GET")
+        with urlopen(req) as res:
+            manifest = json.loads(res.read().decode("utf-8"))
+            assert "uuid" in manifest
 
-    if not skip_run_process:
-        # プロセスが稼働中であることを確認
-        assert process is not None
-        assert process.poll() is None
+        if not skip_run_process:
+            # プロセスが稼働中であることを確認
+            assert process is not None
+            assert process.poll() is None
 
-        # 停止
-        process.terminate()
+    finally:
+        if not skip_run_process and process is not None:
+            # 停止
+            process.terminate()
+            try:
+                process.wait(timeout=5)
+            except TimeoutError:
+                process.kill()
 
 
 if __name__ == "__main__":
